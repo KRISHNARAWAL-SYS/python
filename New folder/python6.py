f198d273b1fb963357e85bc3e4adb037e397f399
krishna rawal <kkrwl575@gmail.com>
2025-04-02 16:46:56 +0530

    commit

---
+++ New folder/python6.py
@@ -0,0 +1,10 @@
+#ternery operator
+#single  lined if-else conditional statement
+# EXAMPLE 1.
+FOOD = input("food :")
+eat = "yes" if FOOD == "dal bati" else "no"
+print(eat)
+
+# example 2.
+food = input("food :")
+print("sweet") if food == "cake" or food == "jalebi" else print("not sweet")
