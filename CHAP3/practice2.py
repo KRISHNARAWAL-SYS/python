bfe17f2414aa5a2a561895d891b9827a297a9e02
krishna rawal <kkrwl575@gmail.com>
2025-04-07 05:15:50 +0530

    commit

--- CHAP3/practice2.py
+++ CHAP3/practice2.py
@@ -2,4 +2,20 @@
 # palindrome of elements.
 # eg; [ 1 ,2 ,3 ,2, 1] ,  [1 , "abc" , "abc" , 1]
 
+list1 = [1 , 2 , 1]
+list2 = [2 , 2 , 3]
 
+copy_list1 = list1.copy()
+list1.reverse()
+copy_list2 = list2.copy()
+list2.reverse()
+
+if(copy_list1 == list1):
+    print('palindrome')
+else :
+    print("invalid")
+
+if(copy_list2== list2):
+    print('palindrome')
+else :
+    print("invalid")
