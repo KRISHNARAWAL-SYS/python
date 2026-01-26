49b5dbc0e08f90ad3c10701f1ae8912fef2cf481
KRISHNA RAWAL <KKRWL575@GMAIL.COM>
2026-01-26 23:31:26 +0530

    comit

---
+++ CHAP6/03_list_using _while.py
@@ -0,0 +1,6 @@
+List = [ "krish", "kumar", "singh", "is", "a", "good", "boy" , 1 , 53,  75]
+i = 0 
+while i < len(List):
+    print(List[i])
+    i += 1
+    
---
+++ CHAP6/04_fo_loop.py
@@ -0,0 +1,6 @@
+#for i in range(3):
+#    print("Hello World")
+
+# print table of 5
+for i in range(0 , 51 , 5):
+    print(i)
