6a07d28bdd87fbea1ed01dd3004893041aa4731e
KRISHNA RAWAL <KKRWL575@GMAIL.COM>
2026-01-29 06:54:25 +0530

    commit

---
+++ HARRYPP/program21.py
@@ -0,0 +1,17 @@
+''' 
+    for n = 3
+        *
+       ***
+      *****
+
+'''
+n = int(input("enter the number of rows :"))
+
+for i in range(1, n+1):
+    # print spaces
+    for j in range(n-i):         # print n-i spaces in ith row
+        print(" ", end="")
+    # print stars
+    for k in range(2*i - 1):      # print 2*i-1 stars in ith row
+        print("*", end="")
+    print("")               # move to next line
