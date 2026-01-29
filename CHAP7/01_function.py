284d87982b8bfb84159c32605d01f1438c491b1a
KRISHNA RAWAL <KKRWL575@GMAIL.COM>
2026-01-30 01:57:39 +0530

    submit

---
+++ CHAP7/01_function.py
@@ -0,0 +1,12 @@
+def avg():                    #function definition
+    a = int(input("enter first number :"))              
+    b = int(input("enter second number :"))
+    c = int(input("enter third number :"))                 
+    average = (a + b + c) / 3
+    print("the average of three numbers is :", average)
+    
+avg()                          #function call
+avg()
+print("thank you ??")
+avg()
+avg()
