ffed2b2a91d880ff4dd2eb4ce5938a97fdbd6708
KRISHNA RAWAL <KKRWL575@GMAIL.COM>
2026-01-30 01:15:49 +0530

    COMMIT

---
+++ HARRYPP/program25.py
@@ -0,0 +1,6 @@
+ # printing of inverse table of a number using for loop
+n = int(input("enter the number for table  :"))
+for i in range(1,11):
+     print(f"{n} X {11-i} = {n*(11-i)}")
+     
+     
