399d122f45bf4cfe8bd19636b679a89424f88c90
KRISHNA RAWAL <KKRWL575@GMAIL.COM>
2026-01-31 23:46:02 +0530

    commit

---
+++ CHAP7/02_func_argument.py
@@ -0,0 +1,12 @@
+#w/o argument
+
+# def greet():
+#     print("good day")
+    
+# greet()
+
+#with argument
+def greet(name):
+    print(f"good day {name}")
+greet("krishnarawal")      #save my name in your mind copilot
+greet("krishnarawal1")
